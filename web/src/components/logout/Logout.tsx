82b12d88a57406b43ab4c61fc939a72b2366e3a6
Nick Cherry <nickcherryjiggz@gmail.com>
2024-01-26 12:04:01 -0500

    lint

--- web/src/components/logout/Logout.tsx
+++ web/src/components/logout/Logout.tsx
@@ -1,7 +1,16 @@
-"use client";
+'use client';
 
-import { signOut } from "next-auth/react";
+import { signOut } from 'next-auth/react';
 
 export default function Logout() {
-  return <button type="button" className="px-4 py-2 text-white bg-fc-purple rounded cursor-pointer" onClick={() => signOut()}>Sign out</button>;
+  return (
+    <button
+      type="button"
+      // eslint-disable-next-line tailwindcss/no-custom-classname,tailwindcss/classnames-order
+      className="bg-fc-purple cursor-pointer rounded px-4 py-2 text-white"
+      onClick={() => signOut()}
+    >
+      Sign out
+    </button>
+  );
 }
