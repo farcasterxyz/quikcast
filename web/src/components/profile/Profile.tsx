cfd439d731c1d9c23ab179ed8c2432073e762ae6
horsefacts <webmaster@terminally.online>
2024-01-26 13:03:13 -0500

    feat: add external links to warpcast

--- web/src/components/profile/Profile.tsx
+++ web/src/components/profile/Profile.tsx
@@ -11,9 +11,19 @@ export function Profile({ user }: ProfileProps) {
   return (
     <div className="flex flex-col p-4">
       <div className="flex flex-row place-items-center">
-        <Avatar user={user} pfpDiameter={72} />
+        <a href={`https://warpcast.com/${user.username}`} target="_blank">
+          <Avatar user={user} size="lg" />
+        </a>
         <div className="ml-4 flex flex-col">
-          <span className="font-bold">{user.display_name}</span>
+          <span className="font-bold">
+            <a
+              href={`https://warpcast.com/${user.username}`}
+              target="_blank"
+              className="hover:underline"
+            >
+              {user.display_name}
+            </a>
+          </span>
           <span className="text-gray-500">@{user.username}</span>
           <div className="mt-2">{user.bio}</div>
         </div>
