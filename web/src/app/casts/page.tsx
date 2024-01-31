7970afaecb7d66304bc40f268567ef163a6ee219
Nick Cherry <nickcherryjiggz@gmail.com>
2024-01-30 19:45:16 -0500

    web cleanup

--- web/src/app/casts/page.tsx
+++ web/src/app/casts/page.tsx
@@ -1,12 +1,12 @@
+import { CastsPage } from '@components/casts/CastsPage';
 import { LandingPage } from '@components/landing/LandingPage';
-import { ProfilePage } from '@components/profile/ProfilePage';
 import { getCurrentUser } from '@lib/auth/getCurrentUser';
 
 export default async function Feed() {
   const user = await getCurrentUser();
 
   if (user) {
-    return <ProfilePage fid={user.fid} />;
+    return <CastsPage fid={user.fid} />;
   }
 
   return <LandingPage />;
