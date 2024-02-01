3d43632d13625c0bab521a42de3eac4bc898d275
horsefacts <webmaster@terminally.online>
2024-02-01 10:36:56 -0500

    Add landing image

--- web/src/components/landing/LandingPage.tsx
+++ web/src/components/landing/LandingPage.tsx
@@ -1,10 +1,12 @@
 import Login from "@components/login/Login";
+import Image from "next/image";
 
 export function LandingPage() {
   return (
     <>
-      <main className="flex flex-col items-center justify-center p-4">
-        <h1 className="pb-1 text-4xl font-bold">Quikcast</h1>
+      <main className="flex flex-col items-center justify-center p-16">
+        <Image src="/landing-logo.webp" width={400} height={400} alt="Farcaster Arch" />
+        <h1 className="pb-1 text-4xl font-extrabold mt-4">Quikcast</h1>
         <h2>A really simple Farcaster app.</h2>
         <div className="mt-4">
           <Login />
